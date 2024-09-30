8cd18f6c43098b80bead68e6c42ea99c5c9abc5e
AKmahim <mdmahimhossen134@gmail.com>
2024-09-30 16:50:18 +0600

    added header in project page

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,9 +1,15 @@
 
 
 const Project = ()=>{
+
     return (
         <div>
-            <h1>from project page</h1>
+            <h1 className="text-center font-medium text-2xl text-[#FCFBFE] mb-5">Here is my All Project</h1>
+            {/* search box */}
+            <div className="flex flex-col justify-center items-center ">
+                <p className=" text-[#FCFBFE] mb-4">6 matching projects</p>
+                <input type="text" className="" />
+            </div>
         </div>
     );
 };
